5fc2e0fc00bff69ba33950f8f5d9edce36bc94c3
ohchansol <haxr369@gmail.com>
2023-11-14 17:41:40 +0900

    create my projects

--- data/projectsData.ts
+++ data/projectsData.ts
@@ -1,19 +1,17 @@
 const projectsData = [
   {
-    title: 'A Search Engine',
-    description: `What if you could look up any information in the world? Webpages, images, videos
-    and more. Google has many features to help you find exactly what you're looking
-    for.`,
-    imgSrc: '/static/images/google.png',
-    href: 'https://www.google.com',
+    title: '여기어때 클론코딩 및 자체 서비스로의 전환',
+    description: `1차로 여기어때 백앤드 서비스 클론코딩을 진행했고, 2차는 여기어때 백앤드 서비스 고도화를 거쳐 클론코딩에서 벗어나
+    자체 서비스를 구축하는 경험과 함께 현업 개발자와의 코드 리뷰를 통한 코드 최적화를 했다.`,
+    imgSrc: '/static/images/ygiy.png',
+    href: 'https://periodic-fisher-f68.notion.site/6c3f6dcea4e4499e9b8065bc415c8d56?pvs=4',
   },
   {
-    title: 'The Time Machine',
-    description: `Imagine being able to travel back in time or to the future. Simple turn the knob
-    to the desired date and press "Go". No more worrying about lost keys or
-    forgotten headphones with this simple yet affordable solution.`,
-    imgSrc: '/static/images/time-machine.jpg',
-    href: '/blog/the-time-machine',
+    title: 'K-d 트리 기반 이미지 인덱싱 데이터베이스 구축 방법과 장치',
+    description: `본 발명은 트리 구조를 활용하여 대량의 이미지 컬렉션 내에서 대표 이미지들을 선택하여 데이터베이스를 구축하고,
+     쿼리 이미지와 유사한 이미지들을 효과적으로 검색하며, 쿼리 이미지를 저장하는 방법과 장치를 제공한다.`,
+    imgSrc: '/static/images/smiliar-img.png',
+    href: 'https://docs.google.com/presentation/d/e/2PACX-1vT188UIH7oJAAjmPGELLNmPrDS0_raRQqG_yBhaQLgRoQawC2qiVycxAJVION9G4_qnoR9P3DcK1mPE/pub?start=false&loop=false&delayms=3000',
   },
 ]
 
