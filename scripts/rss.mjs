1f1dc6389a767ab6dfcb68b4b1095f5db4212217
ohchansol <haxr369@gmail.com>
2025-11-02 01:01:30 +0900

    fix: assert 제거

--- scripts/rss.mjs
+++ scripts/rss.mjs
@@ -1,10 +1,12 @@
-import { writeFileSync, mkdirSync } from 'fs'
+import { writeFileSync, mkdirSync, readFileSync } from 'fs'
 import path from 'path'
 import GithubSlugger from 'github-slugger'
 import { escape } from 'pliny/utils/htmlEscaper.js'
 import siteMetadata from '../data/siteMetadata.js'
-import tagData from '../app/tag-data.json' assert { type: 'json' }
-import { allBlogs } from '../.contentlayer/generated/index.mjs'
+const tagData = JSON.parse(readFileSync(new URL('../app/tag-data.json', import.meta.url), 'utf-8'))
+const allBlogs = JSON.parse(
+  readFileSync(new URL('../.contentlayer/generated/Blog/_index.json', import.meta.url), 'utf-8')
+)
 
 const generateRssItem = (config, post) => `
   <item>
